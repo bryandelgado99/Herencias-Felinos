0e230f208c343f49ed2f753ff3012a425d6c309f
Bryan Delgado <bryand9970@gmail.com>
2023-06-21 19:27:22 -0500

    Creación y definición de la superclase Felinos

--- Felidae/Felinos.java
+++ Felidae/Felinos.java
@@ -1,4 +1,52 @@
 package Felidae;
 
 public class Felinos {
+
+    //Atributos
+    public String nombre_comun;
+    public String nombre_cientifico;
+
+    //Constructores
+
+    public Felinos(String nombre_comun, String nombre_cientifico) {
+        this.nombre_comun = nombre_comun;
+        this.nombre_cientifico = nombre_cientifico;
+    }
+
+    //Métodos
+    public String getNombre_comun() {
+        return nombre_comun;
+    }
+
+    public void setNombre_comun(String nombre_comun) {
+        this.nombre_comun = nombre_comun;
+    }
+
+    public String getNombre_cientifico() {
+        return nombre_cientifico;
+    }
+
+    public void setNombre_cientifico(String nombre_cientifico) {
+        this.nombre_cientifico = nombre_cientifico;
+    }
+
+    private String carnivoro(){
+        System.out.println("Son animales carnivoros");
+        return null;
+    }
+
+    private String herviboro(){
+        System.out.println("Son animales herbivoros");
+        return null;
+    }
+
+    private String ubicacion(){
+        System.out.println("Se distribuyen por todo el planeta");
+        return null;
+    }
+
+    private String extinto(){
+        System.out.println("Sn animales que no se encuentran extintos");
+        return null;
+    }
 }
