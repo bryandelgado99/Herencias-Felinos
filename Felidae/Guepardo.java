5f4fdbc02e4f96a072210580ffb453a9d0bd8649
Bryan Delgado <bryand9970@gmail.com>
2023-06-21 19:26:54 -0500

    Creación y definición de la subclase Guepardo

--- Felidae/Guepardo.java
+++ Felidae/Guepardo.java
@@ -1,4 +1,68 @@
 package Felidae;
 
-public class Guepardo {
+public class Guepardo extends Felinos {
+    //Atributos de clase
+    public String latin;
+    int tamanio, peso;
+
+    //Constructor
+    public Guepardo(String nombre_comun, String nombre_cientifico,
+                 String latin,
+                 int tamanio,
+                 int peso) {
+        super(nombre_comun, nombre_cientifico);
+        this.tamanio = tamanio;
+        this.peso = peso;
+    }
+
+    //Metodos Setters y Getters
+    public String getLatin() {
+        return latin;
+    }
+
+    public void setLatin(String latin) {
+        this.latin = latin;
+    }
+
+    public int getTamanio() {
+        return tamanio;
+    }
+
+    public void setTamanio(int tamanio) {
+        this.tamanio = tamanio;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    //Metodos de la clase
+    private void manchas(){
+        System.out.println("Poseen una cantidad amplia de manchas");
+    }
+    private void alimento(){
+        System.out.println("Su principal alimento son las gacelas.");
+    }
+    private void caza(){
+        System.out.println("Cazan en manada a sus presas");
+    }
+    private void depredadores(){
+        System.out.println("Sus depredadores principales son leones u otros carnivoros grandes");
+    }
+
+    public void imprimirDatos(){
+        System.out.println("\nSu nombre en latin es: "+latin);
+        System.out.println("Su tamaño promedio es "+tamanio);
+        System.out.println("El peso promedio es "+peso);
+        manchas();
+        alimento();
+        caza();
+        depredadores();
+    }
+
 }
+
